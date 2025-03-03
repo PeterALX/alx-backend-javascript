90ac14aa30294fa1d1c9a7b75c27fc108f7521a7
PeterALX <quaypeter25@yahoo.com>
2025-03-03 13:02:25 +0300

    fix 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,8 +4,6 @@ process.stdin.setEncoding('utf8')
 process.stdin.on('readable', () => {
 	const input = process.stdin.read()
 	process.stdout.write('Your name is: ' + input)
-})
-process.stdin.on('end', () => {
 	process.stdout.write('This important software is now closing')
 })
 
