8d122a49f572c39404991ab6fb98ccf59113bab2
PeterALX <quaypeter25@yahoo.com>
2025-03-03 13:13:23 +0300

    ffixxxx

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
-process.stdout.write('Welcome to ALX, what is your name?')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8')
 process.stdin.on('readable', () => {
-	const input = process.stdin.read()
-	process.stdout.write('Your name is: ' + input)
-	process.stdout.write('This important software is now closing')
-})
+	const chunk = process.stdin.read();
 
+	if (chunk) {
+		process.stdout.write(`Your name is: ${chunk}`);
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
+});
