bc7c1542e5e2e974a120b25969a81e3858194123
NinnjA254 <quaypeter@yahoo.com>
2025-03-03 12:55:32 +0300

    done 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to ALX, what is your name?')
+
+process.stdin.setEncoding('utf8')
+process.stdin.on('readable', () => {
+	const input = process.stdin.read()
+	process.stdout.write('Your name is: ' + input)
+})
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing')
+})
+
